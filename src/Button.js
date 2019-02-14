55eb611dea1508d89097282448bb2bdc4ba302c8
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-02-13 21:25:15 -0600

    create click event in Button

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './Button.css';
 
 class Button extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { color: 'red' };
+	}
+	handleClick() {
+		alert('Hello, World... ALERT!');
+	}
 	render() {
 		return (
-			<div className="Button">Submit</div> 
+			<div className="Button" onClick={this.handleClick}>{this.state.color}</div> 
 		);
 	}
 }
